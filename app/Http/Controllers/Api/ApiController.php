b97aebe367fd32920460d307a053d862a28b23d2
Soumojit Saha <wbsoumo@Soumojits-MacBook-Air.local>
2026-09-14 16:49:22 +0530

    Add CORS headers to store/select API endpoint

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -37,7 +37,9 @@ class ApiController extends Controller
                 'is_operational' => true,
                 'closure_reason' => 'Store is open and operational.',
             ]
-        ]);
+        ])->header('Access-Control-Allow-Origin', '*')
+          ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS')
+          ->header('Access-Control-Allow-Headers', '*');
     }
 
     // Get All Categories with ETag & Incremental Sync Support
