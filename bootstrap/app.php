baab62d7e7867371b5db8e0169ef7f055493168f
Soumojit Saha <wbsoumo@Soumojits-MacBook-Air.local>
2026-09-15 15:32:10 +0530

    Exempt api/v1/* routes from CSRF token verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
         $middleware->append(\App\Http\Middleware\CorsMiddleware::class);
         $middleware->alias([
             'admin.only' => \App\Http\Middleware\AdminOnlyMiddleware::class,
