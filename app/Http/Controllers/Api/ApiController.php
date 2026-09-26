5c0ea9a2bbe26d6f2639629951c4ce1bd6252de4
Soumojit Saha <wbsoumo@Soumojits-MacBook-Air.local>
2026-09-26 13:51:29 +0530

    Fix Schema import in ApiController for sliders endpoint

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use App\Services\StoreOperationalService;
 use App\Services\CheckoutValidationService;
 use App\Services\InventoryService;
